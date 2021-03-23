d2122f23fc23e4be0cb4f0460bc25c615a84c421
Gyuris Csaba <gyuris.csaba@vasvari.hu>
2021-03-23 11:39:18 +0100

    Update Fridge.cs

--- model/Fridge.cs
+++ model/Fridge.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wpf_01_01_01_car.model
+{
+    /// <summary>
+    /// Hűtőszekrény
+    /// </summary>
+    class Fridge
+    {
+        /// <summary>
+        /// Raktáros
+        /// </summary>
+        private string storekeeper;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="storekeeper">A hűtőszekrészt bevételező vagy kiadó raktáros</param>
+        public Fridge(string storekeeper)
+        {
+            this.storekeeper = storekeeper;
+        }
+
+        /// <summary>
+        /// Hűtőszekrész bevételezése a raktárba
+        /// </summary>
+        public string TakesToWarehouse()
+        {
+            return storekeeper + " bevételez egy hűtőszekrényt a raktárba.";
+        }
+        /// <summary>
+        /// Hűtőszekrész kiadása a raktárból
+        /// </summary>
+        public string GoOutFromWarehouseString()
+        {
+            return storekeeper + " kiad egy hűtőszekrényt a raktárból.";
+        }
+    }
+}
 
